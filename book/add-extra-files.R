98913a3e5a06029f472ccb9344fe56861fb76640
Koen Hufkens <koen.hufkens@gmail.com>
2023-03-12 22:03:18 +0100

    update post processing

--- book/add-extra-files.R
+++ book/add-extra-files.R
@@ -1,6 +1,6 @@
 # Copy the required webr js files into the build directory
-file.copy("modules/webr-serviceworker.js", "_book/webr-serviceworker.js")
-file.copy("modules/webr-worker.js", "_book/webr-worker.js")
+file.copy("_extensions/coatless/webr/webr-serviceworker.js", "_book/webr-serviceworker.js")
+file.copy("_extensions/coatless/webr/webr-worker.js", "_book/webr-worker.js")
 
 # Copy the custom headers file for netlify to improve speed and security
 # See https://docs.r-wasm.org/webr/latest/serving.html
